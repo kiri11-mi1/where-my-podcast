1326b51325d7e6983f5917edb353b5b7ab7e0197
kiri11-mi1 <mil.kirillka@mail.ru>
2020-08-28 05:39:01 +0700

    Использование веб хуков

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sslify import SSLify
+import json
+from bot import Bot
+from config import Config
 
 app = Flask(__name__)
 sslify = SSLify(app)
+cfg = Config()
 
-@app.route('/')
+bot = Bot(cfg.TOKEN)
+webhook = bot.set_webhook(cfg.HOST)
+if not webhook['ok']:
+    print(f"Description --> {webhook['description']}")
+    raise 'Webhook Error'
+
+def write_json(data, filename='response.json'):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return '<h1>Hello, World!</h1>'
+    if request.method == 'POST':
+        response = request.get_json()
+        write_json(response)
+        return jsonify(response)
+    return '<h1>Wellcome</h1>'
+    
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
