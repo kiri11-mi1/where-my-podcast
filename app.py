80e332f5dcbd8d6b4ae2246b9ed9b07024ec0e2c
kiri11-mi1 <mil.kirillka@mail.ru>
2020-08-29 05:56:56 +0700

    Добавил обьект конфига в парметры

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sslify import SSLify
 import json
 from bot import Bot
 from config import Config
+from handler import handle
 
 app = Flask(__name__)
 sslify = SSLify(app)
@@ -14,16 +15,13 @@ if not webhook['ok']:
     print(f"Description --> {webhook['description']}")
     raise 'Webhook Error'
 
-def write_json(data, filename='response.json'):
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
-
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         response = request.get_json()
-        write_json(response)
+        handle(response, bot, cfg)
+        #write_json(response)
         return jsonify(response)
     return '<h1>Wellcome</h1>'
     
