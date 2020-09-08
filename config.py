0e99d02e5ceeb21e5b921121aa2ae34eb23f245f
kiri11-mi1 <mil.kirillka@mail.ru>
2020-09-09 04:43:15 +0700

    Добавил username админского акка

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     TOKEN = os.environ.get('API_TOKEN')
     HOST = os.environ.get('URL')
+    ADMIN = os.environ.get('ADMIN')
+    
     CONTENT_TYPE = None
     CONTENT_URL = None
     CONTENT = None
     CHAT_ID = None
-
-    # 26\ CSGO\ -\ Вертушки\ авапера.webm
