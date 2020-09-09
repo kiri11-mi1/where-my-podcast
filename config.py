b9eae458d93f50b1f26f657d604d018d82e25d1d
kiri11-mi1 <mil.kirillka@mail.ru>
2020-09-09 20:33:52 +0700

    Оформил код по стандарту

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
 
+
 class Config:
     TOKEN = os.environ.get('API_TOKEN')
     HOST = os.environ.get('URL')
     ADMIN = os.environ.get('ADMIN')
-    
-    CONTENT_TYPE = None
-    CONTENT_URL = None
-    CONTENT = None
-    CHAT_ID = None
