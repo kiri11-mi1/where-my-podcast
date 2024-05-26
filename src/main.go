a47566a8a829ea763b947a11797fa8e30f37c242
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-05-26 19:43:12 +0700

    simple bot

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	pref := tg.Settings{
-		Token:  GetVal().TelegramToken,
+		Token:  GetEnv().TelegramToken,
 		Poller: &tg.LongPoller{Timeout: 10 * time.Second},
 	}
 	b, err := tg.NewBot(pref)
@@ -18,7 +18,10 @@ func main() {
 		return
 	}
 
-	// b.Handle("/start", handlerManager.HandleVPN)
+	b.Handle(tg.OnText, func(c tg.Context) error {
+		_, err := c.Bot().Send(c.Recipient(), "Hello, world!")
+		return err
+	})
 
 	log.Println(b.Me.Username, "start working...")
 	b.Start()
