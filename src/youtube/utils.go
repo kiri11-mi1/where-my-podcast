3a649608daa7124ef725702fd2a00754e398ece2
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-05-26 22:30:59 +0700

    init utils

---
+++ src/youtube/utils.go
@@ -0,0 +1,11 @@
+package youtube
+
+func IsValidLink(url string) bool {
+	// Checking if youtube url is valid
+	return true
+}
+
+func GetYTId(url string) (string, error) {
+	// Fetching video id from youtube url
+	return "id", nil
+}
