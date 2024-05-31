0b310a04ce68a7c70533851dcdf36391e460abd0
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-05-31 23:27:59 +0700

    add loginc into utils

--- src/youtube/utils.go
+++ src/youtube/utils.go
@@ -1,11 +1,38 @@
 package youtube
 
-func IsValidLink(url string) bool {
+import (
+	"net/url"
+	"regexp"
+)
+
+func IsValidLink(link string) bool {
 	// Checking if youtube url is valid
-	return true
+	parsedURL, err := url.Parse(link)
+	if err != nil {
+		return false
+	}
+
+	if parsedURL.Host != "www.youtube.com" && parsedURL.Host != "youtube.com" && parsedURL.Host != "youtu.be" {
+		return false
+	}
+
+	if parsedURL.Host == "youtu.be" {
+		match, _ := regexp.MatchString(`^[a-zA-Z0-9_-]{11}$`, parsedURL.Path[1:])
+		return match
+	}
+
+	if parsedURL.Path == "/watch" {
+		queryParams := parsedURL.Query()
+		if val, ok := queryParams["v"]; ok {
+			match, _ := regexp.MatchString(`^[a-zA-Z0-9_-]{11}$`, val[0])
+			return match
+		}
+	}
+
+	return false
 }
 
-func GetYTId(url string) (string, error) {
+func Link2Id(url string) (string, error) {
 	// Fetching video id from youtube url
 	return "id", nil
 }
