43d9821e8a03197a59cf640dc80df905d464beb6
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-06-01 00:42:21 +0700

    add tests to get id fron link

--- src/tests/test_youtube/utils_test.go
+++ src/tests/test_youtube/utils_test.go
@@ -29,3 +29,25 @@ func TestUtils_IsValidLink(t *testing.T) {
 		assert.Equal(t, false, actual)
 	})
 }
+
+func TestUtils_Link2Id(t *testing.T) {
+	t.Run("success fetch id", func(t *testing.T) {
+		link := "https://www.youtube.com/watch?v=v6rMPOlPzpy"
+		expected := "v6rMPOlPzpy"
+		actual, err := youtube.Link2Id(link)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	})
+	t.Run("success fetch id", func(t *testing.T) {
+		link := "https://youtu.be/GGq0BIF0GXc?si=2jXbI4H_JkqA2tiB"
+		expected := "GGq0BIF0GXc"
+		actual, err := youtube.Link2Id(link)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	})
+	t.Run("fail fetch id", func(t *testing.T) {
+		link := "https://www.youtube.com/embed/dQw4w9WgXcQ"
+		_, err := youtube.Link2Id(link)
+		assert.ErrorIs(t, err, youtube.ErrInvalidYoutubeLink)
+	})
+}
