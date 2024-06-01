9f5e6df4d0308ec5926a418eff4849641c287ede
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-06-01 17:07:09 +0700

    add dowloader logic

--- src/youtube/downloader.go
+++ src/youtube/downloader.go
@@ -1,12 +1,30 @@
 package youtube
 
-type Downloader struct{}
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type Downloader struct {
+	folder string
+}
 
 func NewDownloader() *Downloader {
 	return &Downloader{}
 }
 
-func (d *Downloader) Download(url string) {
-	// TODO: скачивание ролика
-	panic("not implemented")
+func (d *Downloader) Download(link, video_id string) (string, error) {
+	outputPath := fmt.Sprintf("%s/%s.mp3", d.folder, video_id)
+	defer os.Remove(outputPath)
+	cmd := exec.Command("yt-dlp", "-x", "--audio-format", "mp3", "-o", d.folder, link)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return "", ErrVideoDownloadFailed
+	}
+	return outputPath, nil
 }
