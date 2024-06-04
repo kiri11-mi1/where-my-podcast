873ab698b9789f4896d16c79ded780c2ff64f131
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-06-04 17:42:59 +0700

    new errors

--- src/youtube/errors.go
+++ src/youtube/errors.go
@@ -4,3 +4,6 @@ import "errors"
 
 var ErrInvalidYoutubeLink = errors.New("InvalidLink")
 var ErrVideoDownloadFailed = errors.New("VideoDownloadFailed")
+var ErrFetchingVideoInfoFailed = errors.New("FetchingVideoInfoFailed")
+var ErrParseHTMLFailed = errors.New("ParseHTMLFailed")
+var ErrChannelNotFound = errors.New("ChannelNotFound")
