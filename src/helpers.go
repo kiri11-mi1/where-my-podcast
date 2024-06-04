ab5ff35ea9e24b674726ba652790b52dc2306856
kiri11-mi1 <kirill.mil.0310@mail.ru>
2024-06-04 21:57:41 +0700

    add helper

---
+++ src/helpers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	yt "where-my-podcast/youtube"
+
+	tg "gopkg.in/telebot.v3"
+)
+
+func MakeAudio(v yt.Video, fileStr string) *tg.Audio {
+	file := tg.FromDisk(fileStr)
+	thumbnail := &tg.Photo{File: tg.FromURL(v.Thumbnail)}
+	return &tg.Audio{File: file, Performer: v.Channel, Title: v.Title, Thumbnail: thumbnail}
+}
